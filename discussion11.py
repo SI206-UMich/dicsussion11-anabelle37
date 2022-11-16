60cebd073592ea32486f14ea8f2c3a5c7d248b91
Anabelle Phillips <anphilli@umich.edu>
2022-11-16 16:01:00 -0500

    task 2

--- discussion11.py
+++ discussion11.py
@@ -39,12 +39,16 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
+    cur = conn.cursor()
+    cur.execute('DROP TABLE IF EXISTS Patients')
+    cur.execute('CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id NUMBER, age INTEGER, cuteness INTEGER, aggressiveness NUMBER)')
+    conn.commit()
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
+    cur = conn.cursor()
+    cur.execute('INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES (?,?,?,?,?,?)'(0,'Fluffle',0,3,90,100))
+    conn.commit()
     
 
 # TASK 2
@@ -59,7 +63,18 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
+    count = 1
+    for data in json_data:
+        pet_id = count 
+        count+=1
+        name = data['name']
+        cur.execute('SELECT id FROM Species WHERE title = ?', (data['species'],))
+        species_id= cur.fetchone[0]
+        age = int(data['age'])
+        cuteness = int(data['cuteness'])
+        aggressiveness = int(data['aggressiveness']) 
+        cur.execute('INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) VALUES(?,?,?,?,?,?)', (pet_id, name, species_id, age, cuteness, aggressiveness))
+    conn.commit()
 
 
 # TASK 3
